e7885301f273967433ec75f69cc34b4679a575e4
SnugTop <camjdaly@gmail.com>
2023-12-23 15:48:30 -0500

    Implement Empolyee Class and interface

--- src/main/java/payroll/Employee.java
+++ src/main/java/payroll/Employee.java
@@ -6,9 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * This class is a standard representation of an entity in a JPA-based application. 
+ * It's designed to be mapped to a table in a relational database, where each instance 
+ * of Employee corresponds to a row in the table, and each field of the class corresponds 
+ * to a column in that row.
+ */
+
+@Entity
 public class Employee {
     
-
     private @Id @GeneratedValue Long id;
     private String name;
     private String role;
@@ -43,4 +50,31 @@ public class Employee {
     public void setRole(String role) {
         this.role = role;
     }
+
+    @Override
+    public boolean equals(Object o){
+
+        if(this == o)
+            return true;
+
+        if(!(o instanceof Employee))
+            return false;
+
+        Employee employee = (Employee) o;
+
+        return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name) 
+        && Objects.equals(this.role, employee.role);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name, this.role);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+    }
+
 }
