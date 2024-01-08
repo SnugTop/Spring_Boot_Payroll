a2a5fdeee3e87d45b61dd85f51e9b4446054fe95
SnugTop <camjdaly@gmail.com>
2024-01-08 16:55:42 -0500

    Finish updating controll for PUT & Delete. Also Add Order Class

--- src/main/java/payroll/controller/EmployeeController.java
+++ src/main/java/payroll/controller/EmployeeController.java
@@ -72,24 +72,33 @@ class EmployeeController {
 
     // Update an existing employee or create a new one if it doesn't exist
     @PutMapping("/employees/{id}")
-    Employee replacEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+    ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 
-        return repository.findById(id)
+        Employee updatedEmployee = repository.findById(id) //
                 .map(employee -> {
                     employee.setName(newEmployee.getName());
                     employee.setRole(newEmployee.getRole());
                     return repository.save(employee);
-                })
+                }) //
                 .orElseGet(() -> {
                     newEmployee.setId(id);
                     return repository.save(newEmployee);
                 });
+
+        EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+
+        return ResponseEntity //
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+                .body(entityModel);
     }
 
     // Delete an employee
     @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+
         repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
