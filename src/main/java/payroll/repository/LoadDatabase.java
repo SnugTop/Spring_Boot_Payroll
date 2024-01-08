d1067b3a020ca26cccbb94562b5307e7bee453a7
SnugTop <camjdaly@gmail.com>
2024-01-08 16:41:50 -0500

    Old-way VS New-way

--- src/main/java/payroll/repository/LoadDatabase.java
+++ src/main/java/payroll/repository/LoadDatabase.java
@@ -10,15 +10,15 @@ import payroll.model.Employee;
 
 @Configuration
 public class LoadDatabase {
-    
+
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
     CommandLineRunner initDatabase(EmployeeRepository repository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
-            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
-            
+            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+
         };
     }
 }
